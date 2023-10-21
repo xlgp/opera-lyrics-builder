49a31e655ec0e9da63e96ba5c62c3f02f828656e
心灵的孤僻 <zhang15733@sina.cn>
2023-10-21 14:36:23 +0800

    feat: 添加链接为_blank

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -2,13 +2,13 @@
     <el-menu :default-active="activeIndex" mode="horizontal" :ellipsis="false" @select="handleSelect">
         <el-menu-item index="title">戏曲字幕 唱词制作</el-menu-item>
         <div class="flex-grow" />
-        <el-menu-item index="extentions"><a href="https://gitee.com/xlgp/XiquZimuExtensions">插件</a></el-menu-item>
-        <el-menu-item index="oldVersion"><a href="https://xlgp.gitee.io/changci.html">旧版</a></el-menu-item>
-        <el-menu-item index="gitee"><a href="https://gitee.com/xlgp/opera-lyrics-builder">
+        <el-menu-item index="extentions"><a target="_blank" href="https://gitee.com/xlgp/XiquZimuExtensions">插件</a></el-menu-item>
+        <el-menu-item index="oldVersion"><a target="_blank" href="https://xlgp.gitee.io/changci.html">旧版</a></el-menu-item>
+        <el-menu-item index="gitee"><a target="_blank" href="https://gitee.com/xlgp/opera-lyrics-builder">
                 <img src="https://gitee.com/static/images/logo-en.svg" alt="Gitee" width="32"
                     style="vertical-align: middle;" />
             </a></el-menu-item>
-        <el-menu-item index="gitee"><a href="https://github.com/xlgp/opera-lyrics-builder">
+        <el-menu-item index="gitee"><a target="_blank" href="https://github.com/xlgp/opera-lyrics-builder">
                 <img src="https://gitee.com/xlgp/static/raw/master/github-logos/github-mark.svg" alt="Github" width="32"
                     style="vertical-align: middle;" />
             </a></el-menu-item>
