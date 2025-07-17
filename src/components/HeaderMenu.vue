55cfb1e917bdb39d9725b916b4d9ca7bbd7e91c1
hzsn <hzsn-csh@qq.com>
2025-07-17 13:35:14 +0800

    feat: 新增使用说明

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -2,6 +2,7 @@
     <el-menu :default-active="activeIndex" mode="horizontal" :ellipsis="false" @select="handleSelect">
         <el-menu-item index="title">戏曲字幕 唱词制作</el-menu-item>
         <div class="flex-grow" />
+        <el-menu-item index="guide"><guide-viewer/></el-menu-item>
         <el-menu-item index="extentions"><a target="_blank" href="https://gitee.com/xlgp/XiquZimuExtensions">插件</a></el-menu-item>
         <el-menu-item index="oldVersion"><a target="_blank" href="https://xlgp.github.io/opera-lyrics-builder/changci.html">旧版</a></el-menu-item>
         <el-menu-item index="gitee"><a target="_blank" href="https://gitee.com/xlgp/opera-lyrics-builder">
@@ -12,16 +13,13 @@
                 <img src="https://gitee.com/xlgp/static/raw/master/github-logos/github-mark.svg" alt="Github" width="30"
                     style="vertical-align: middle;" />
             </a></el-menu-item>
-    </el-menu>
+    </el-menu>    
 </template>
   
 <script lang="ts" setup>
 import { ref } from 'vue'
 
 const activeIndex = ref('title')
-const handleSelect = (key: string, keyPath: string[]) => {
-    console.log(key, keyPath)
-}
 </script>
   
 <style>
---
+++ src/components/guideViewer.vue
@@ -0,0 +1,16 @@
+<template>
+<el-button @click="showPreview = true" text>说明</el-button>
+ <el-image-viewer
+        v-if="showPreview"
+        :url-list="srcList"
+        show-progress
+        @close="showPreview = false"
+      />
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const showPreview = ref(false)
+const srcList = ["https://gitlab.com/xlgp/static/-/raw/main/opera-lyrics-builder/1.jpg"];
+
+</script>
