91529789aecb70a582c76a225b8da093e6772c9f
hzsn <hzsn-csh@qq.com>
2025-07-17 15:00:08 +0800

    refactor: 懒加载使用说明图片

--- src/components/guideViewer.vue
+++ src/components/guideViewer.vue
@@ -1,16 +1,29 @@
 <template>
-<el-button @click="showPreview = true" text>说明</el-button>
- <el-image-viewer
-        v-if="showPreview"
-        :url-list="srcList"
-        show-progress
-        @close="showPreview = false"
-      />
+  <el-button @click="showPreview = true" text
+    >说明
+    <el-image :src="srcList[0]" lazy style="width: 10px; height: 10px">
+      <template #placeholder>
+        <div class="image-slot">...</div>
+      </template>
+      <template #error>
+        <div class="image-slot">
+          <el-icon><icon-picture /></el-icon>
+        </div>
+      </template>
+    </el-image>
+  </el-button>
+  <el-image-viewer
+    v-if="showPreview"
+    :url-list="srcList"
+    show-progress
+    @close="showPreview = false"
+  />
 </template>
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
-const showPreview = ref(false)
-const srcList = ["https://gitlab.com/xlgp/static/-/raw/main/opera-lyrics-builder/1.jpg"];
-
+const showPreview = ref(false);
+const srcList = [
+  "https://gitlab.com/xlgp/static/-/raw/main/opera-lyrics-builder/1.jpg",
+];
 </script>
