2b484fffa1d61beed99231bf552b1565a9853571
hzsn <hzsn-csh@qq.com>
2025-07-17 15:27:48 +0800

    fix: 修复版本号显示异常

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -1,10 +1,12 @@
 <template>
-    <el-menu :default-active="activeIndex" mode="horizontal" :ellipsis="false" @select="handleSelect">
+    <el-menu mode="horizontal" :ellipsis="false">
         <el-menu-item index="title">戏曲字幕 唱词制作<small>({{ version }})</small></el-menu-item>
         <div class="flex-grow" />
-        <el-menu-item index="guide"><guide-viewer/></el-menu-item>
-        <el-menu-item index="extentions"><a target="_blank" href="https://gitee.com/xlgp/XiquZimuExtensions">插件</a></el-menu-item>
-        <el-menu-item index="oldVersion"><a target="_blank" href="https://xlgp.github.io/opera-lyrics-builder/changci.html">旧版</a></el-menu-item>
+        <el-menu-item index="guide"><guide-viewer /></el-menu-item>
+        <el-menu-item index="extentions"><a target="_blank"
+                href="https://gitee.com/xlgp/XiquZimuExtensions">插件</a></el-menu-item>
+        <el-menu-item index="oldVersion"><a target="_blank"
+                href="https://xlgp.github.io/opera-lyrics-builder/changci.html">旧版</a></el-menu-item>
         <el-menu-item index="gitee"><a target="_blank" href="https://gitee.com/xlgp/opera-lyrics-builder">
                 <img src="https://gitee.com/static/images/logo-en.svg" alt="Gitee" width="30"
                     style="vertical-align: middle;" />
@@ -13,15 +15,14 @@
                 <img src="https://gitee.com/xlgp/static/raw/master/github-logos/github-mark.svg" alt="Github" width="30"
                     style="vertical-align: middle;" />
             </a></el-menu-item>
-    </el-menu>    
+    </el-menu>
 </template>
-  
+
 <script lang="ts" setup>
-import { ref } from 'vue'
 
 const version = __APP_VERSION__;
 </script>
-  
+
 <style scoped>
 .flex-grow {
     flex-grow: 1;
@@ -32,6 +33,7 @@ const version = __APP_VERSION__;
     color: var(--el-color-primary-light-9);
 }
 
-small {margin-left: 10px;}
+small {
+    margin-left: 10px;
+}
 </style>
-  
